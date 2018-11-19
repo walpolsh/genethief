699035e8af8b42a6daf45633fc0923469035e007
Paul Walsh <twentytwentyk@live.com>
2018-11-19 16:59:50 -0500

    first

---
+++ genethief.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+URL = 'http://www.readingsbygenevieve.com/contactgenevieve.htm'
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+NAME
+PHONE
+EMAIL
+TIME
+INFO
